aead9e2a77dca1d4c2da7cd894aa381b8d59bcc4
Marion Lalonde <lalondemarion.perso@gmail.com>
2022-10-12 13:20:39 +0200

    first introducte to react... changing component

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloWorld = () => (
+  <div>
+    <h1>Hello React World 👋🏻</h1>
+    <p>
+      I'm learning React. It looks like easy and difficult in the same time...
+    </p>
+  </div>
+);
+
+export default HelloWorld;
